1cbe01e458b875b2536a3fc3b23a4f5587929eb1
jloescher <jonathan@jloescher.com>
2023-01-11 17:06:03 -0500

    Created Game Logic

--- main.py
+++ main.py
@@ -53,3 +53,138 @@ class Computer(Player):
             "error": None,
             "choice": self.choice,
         }
+
+
+# Create the Game Logic
+class Game:
+    def __init__(self, player: Human, computer: Computer, run: bool):
+        self.player = player
+        self.computer = computer
+        self.run = True
+
+    def check_winner(self):
+        if self.player.choice == self.computer.choice:
+            return {
+                "winner": False,
+                "message": "It's a tie!",
+            }
+        else:
+            match self.player.choice:
+                case "rock":
+                    if self.computer.choice == "scissors":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "paper":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "lizard":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "spock":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                case "paper":
+                    if self.computer.choice == "rock":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "scissors":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "lizard":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "spock":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                case "scissors":
+                    if self.computer.choice == "rock":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "paper":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "lizard":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "spock":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                case "lizard":
+                    if self.computer.choice == "rock":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "paper":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "scissors":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "spock":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                case "spock":
+                    if self.computer.choice == "rock":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "paper":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                    elif self.computer.choice == "scissors":
+                        return {
+                            "winner": True,
+                            "message": "You win!",
+                        }
+                    elif self.computer.choice == "lizard":
+                        return {
+                            "winner": False,
+                            "message": "Computer wins!",
+                        }
+                case _:
+                    return "Invalid input! You have not entered rock, paper, scissors, lizard or spock"
+
+    def run_game(self):
+        while self.run:
+            self.player.get_input()
+            self.computer.get_input()
+            computer_choice = self.computer.choice
+            print(f"The computer chose {computer_choice.title()}")
+            check = self.check_winner()
+            if check["winner"] == True:
+                self.run = False
+            print(check["message"])
