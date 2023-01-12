ab825e675fbacc6bdc4e4e97d3a47d7dda8da91a
jloescher <jonathan@jloescher.com>
2023-01-12 15:12:15 -0500

    Removed CORS, will be required fro dev environment Hosting production build of react in FastAPI

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+# from fastapi.middleware.cors import CORSMiddleware
 from game import Human, Computer, Game
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+#     "http://localhost:54474",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1:54474",
+# ]
 
 app = FastAPI(title="RPSLS API")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+app.mount("/static", StaticFiles(directory="frontend/build/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return open(
+        "frontend/build/index.html",
+        "r",
+    ).read()
 
 
 @app.post("/play")
